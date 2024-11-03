8d9dcd87f7d83ba4a2c242a21e8f9ce36f443c00
Shamil Krut <gmific@yandex.ru>
2024-11-03 05:52:22 -0800

    Update num1.cpp

--- num1.cpp
+++ num1.cpp
@@ -1,3 +1,4 @@
+// !! студента с меньшим индексом буду называть верхним, а с большим индексом - нижним(как в воображаемом журнале)
 #include <iostream>
 #include <vector>
 
@@ -7,14 +8,27 @@ struct Student{
     string GroupId;
     vector<unsigned> Ratings;
     vector<string> Subject;
-    Student* next;
 };
-#include "header.hpp"
 
-
-void SortByName(vector<Student>&students);
-
-
+void SortByName(vector<Student>&students)
+{
+    unsigned long number;
+    number = students.size(); // количество студентов
+    Student Ghost; // ненастоящий студент для обмена местами двух действительных студентов
+    for(int i = 0; i < number; ++i) // перебираем каждого студента
+    {
+        for(int j = i; j < number; ++j) // перебираем для данного студента пару, с которой он не сравнивался (в каждой итерации индекс j >= i ==> по условию(строка 6) i-тый студент всегда будет верхним по отношению к j-тому)
+        {
+            if(int(students[j].Name[0]) < int(students[i].Name[0])) // если первая буква имени верхнего студента по алфавиту идет после буквы имени нижнего студента
+                // int("A") = 65 int("Z") = 95, строчные буквы кодируются в таком же порядке, но в случае с именами они не нужны int("a") = 97 int("z") = 122
+            {
+                Ghost = students[i];
+                students[i] = students[j]; // меняем
+                students[j] = Ghost;
+            }
+        }
+    }
+}
 
 int main()
 {
