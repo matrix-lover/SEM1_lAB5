34b9f97f897adabbe5b9a4e0dec7d3ab4e453eb2
Shamil Krut <gmific@yandex.ru>
2024-11-03 05:44:20 -0800

    Update num4.cpp

--- num4.cpp
+++ num4.cpp
@@ -7,11 +7,30 @@ struct Student{
     string GroupId;
     vector<unsigned> Ratings;
     vector<string> Subject;
-    Student* next;
-};
-#include "header.hpp"
 
-size_t CountExellent(const vector<Student>&students);
+};
+
+size_t CountExellent(const vector<Student>&students)
+{
+    size_t count = 0;
+    unsigned long number;
+    number = students.size();
+    
+    for(int i = 0; i < number; ++i)
+    {
+        for(int rat_num = 0; rat_num < students[i].Ratings.size(); ++rat_num) // перебираем оценки
+        {
+            if(students[i].Ratings[rat_num] != 5) // какая - то из оценок не 5
+            {
+                count += 1;
+                break; // чтобы не считать все оценки, не равные 5
+            }
+        }
+    }
+    
+    return number - count; // все - неотличники
+}
+
 
 int main()
 {
