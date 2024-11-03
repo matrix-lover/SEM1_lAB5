9c5a9caf0519846a70524d661197eec4142623d2
Shamil Krut <gmific@yandex.ru>
2024-11-03 05:43:12 -0800

    Update num5.cpp

--- num5.cpp
+++ num5.cpp
@@ -7,11 +7,26 @@ struct Student{
     string GroupId;
     vector<unsigned> Ratings;
     vector<string> Subject;
-    Student* next;
 };
-#include "header.hpp"
 
-vector<Student> VectorMathExellent(const vector<Student>&students);
+vector<Student> VectorMathExellent(const vector<Student>&students)
+{
+    vector<Student> MathExellent;
+    unsigned long number;
+    number = students.size(); // количество студентов
+    int index = 0; // индекс предмета в векторе Subject
+    
+    for(int i = 0; i < number; ++i) // перебираем студентов
+    {
+        if(students[i].Ratings[index] == 5)
+        {
+            MathExellent.push_back(students[i]);
+        }
+    }
+    
+    return MathExellent;
+}
+
 
 int main()
 {
